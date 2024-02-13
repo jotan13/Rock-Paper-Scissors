0136d220d47238f261d2ffc8f012e675e31f6a07
jonathan <60315144+jotan13@users.noreply.github.com>
2024-02-13 23:46:59 +0800

    Add files via upload

---
+++ P4_RockPaperScissors.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    internal class Project4
+    {
+        static Random random = new Random();
+        static string[] choice = { "rock", "paper", "scissors" };
+        static void Main(string[] args)
+        {
+            string compChoice = computerChoice();
+            string playerChoice;
+
+            Console.WriteLine("Welcome to rock, paper, scissors!");
+
+            Console.Write("Your choice: ");
+            playerChoice = Console.ReadLine().ToLower();
+            try
+            {
+                while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors")
+                {
+                    Console.Write("Invalid input! Please check for spelling errors and retry: ");
+                    playerChoice = Console.ReadLine().ToLower();
+                }
+
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return;
+            }
+
+            Console.WriteLine("Computer's choice: " + compChoice);
+
+            if (String.Compare(playerChoice, compChoice) == 0)
+            {
+                Console.WriteLine("It's a draw!");
+                return;
+            } else if ((playerChoice == "scissors" && compChoice == "paper") || 
+                        (playerChoice == "rock" && compChoice == "scissors") || 
+                        (playerChoice == "paper" && compChoice == "rock"))
+            {
+                Console.WriteLine("You win!");
+                return;
+            } else
+            {
+                Console.WriteLine("You lose!");
+                return;
+            }
+        }
+
+        static string computerChoice()
+        {
+            string compChoice = choice[random.Next(0, choice.Length - 1)];
+            return compChoice;
+        }
+    }
+}
